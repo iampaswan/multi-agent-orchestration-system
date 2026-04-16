e80467697ce765ca4de99090c1f135483316a548
user <krishna1119kumar@gmail.com>
2026-04-16 11:52:16 +0530

    gastwon

--- frontend/src/components/ResearchBox.tsx
+++ frontend/src/components/ResearchBox.tsx
@@ -131,7 +131,7 @@ const ResearchBox: React.FC = () => {
 
                <button
                   onClick={handleResearch}
-                  className=" px-5 py-2 rounded-lg font-semibold border border-gray-600 focus:outline-none hover:focus:ring-2 focus:ring-blue-500"
+                  className=" px-5 py-2 rounded-lg font-semibold border border-gray-600 hover:focus:outline-none hover:focus:ring-2 focus:ring-blue-500"
                >
                   Send
                </button>
